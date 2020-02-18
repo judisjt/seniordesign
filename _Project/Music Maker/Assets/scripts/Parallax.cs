146e70a46b3570f5892beec4e0f49f7a5fb2d569
judisjt <31445081+judisjt@users.noreply.github.com>
2020-02-18 16:13:00 -0500

    new commit
    asdfas

---
+++ _Project/Music Maker/Assets/scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (cam.transform.position.x * parallaxEffet);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    }
+}
