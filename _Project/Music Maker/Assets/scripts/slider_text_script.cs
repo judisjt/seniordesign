4755e86c2ca5570b0a40c3634e408720feafc663
judisjt <31445081+judisjt@users.noreply.github.com>
2020-02-18 18:32:57 -0500

    added slider working
    asdfads

---
+++ _Project/Music Maker/Assets/scripts/slider_text_script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class slider_text_script : MonoBehaviour
+{
+    public Slider superSlider;
+    TMP_Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+         text = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var superSliderValuString = superSlider.value.ToString();
+        text.text = superSliderValuString;
+    }
+
+}
