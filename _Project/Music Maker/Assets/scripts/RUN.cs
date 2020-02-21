3998b247effa375026ccffe00aa0e1ad04f4c975
judisjt <31445081+judisjt@users.noreply.github.com>
2020-02-21 17:43:14 -0500

    scrolling works with sheet
    asdfadsf

--- _Project/Music Maker/Assets/scripts/RUN.cs
+++ _Project/Music Maker/Assets/scripts/RUN.cs
@@ -1,26 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RUN : MonoBehaviour
 {
-    private float speed;
+    public float speed;
     public float bpm;
     public Transform sheet;
     public Transform[] notes;
     public GameObject currentStaff;
+    public Transform beat1;
+    public Transform beat2;
+    public float distance;
+    public Slider slider;
 
     // Start is called before the first frame update
     void Start()
     {
-        float beatspersecond = bpm / 60;
-        float secondsperbeat = 1 / beatspersecond;
-        speed = 215 / secondsperbeat;
+        bpm = slider.value;
+        distance = beat2.transform.position.x - beat1.transform.position.x;
     }
 
     // Update is called once per frame
+
     void Update()
     {
         sheet.transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    public void StartScrolling()
+    {
+        speed = (distance * bpm / 60) * 2;
     }
 }
